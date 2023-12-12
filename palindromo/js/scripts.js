615a613b476a4ba881fb830d7e565673562cb78d
MirkoCifelli <ciffo4@hotmail.it>
2023-12-12 16:09:46 +0100

    Palindroma

---
+++ palindromo/js/scripts.js
@@ -0,0 +1,27 @@
+// Palidroma
+// Chiedere all'utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+const parolaInserita = prompt('Inserisci una parola');
+console.log('parolaInserita', parolaInserita, typeof parolaInserita)
+
+const parolaPalindroma = palindroma(parolaInserita);
+console.log('parolaPalindroma', parolaPalindroma, typeof parolaPalindroma)
+
+
+if(parolaInserita == parolaPalindroma){
+    alert('Palindroma');
+}
+else{
+    alert('non e palindroma');
+}
+
+
+
+function palindroma(parola) {
+    let  parolaReverse = ('');
+    for (let i = parola.length -1; i >= 0; i--){
+        parolaReverse += parola[i];    
+    }
+    return parolaReverse;
+}
